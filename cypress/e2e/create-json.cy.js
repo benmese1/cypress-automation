1baaa6306519ce8a80c6f870ce623dfab1095e61
Morin <morin_ja@am.adecco.net>
2022-08-23 09:30:09 -0600

    some changes I forgot to make to my own branch, and made on master instead

--- cypress/e2e/create-json.cy.js
+++ cypress/e2e/create-json.cy.js
@@ -1,11 +1,13 @@
 // import data from '../fixtures/data-example.json'
 import {dataCreation} from '../support/factory/main.js'
+import {additionalDataCreation} from '../support/factory/main.js'
 
 describe('writing data to json file', () => {
     let fileName = 'dataTest.json';
 
     before(() => {
         dataCreation(fileName);
+        // additionalDataCreation(fileName)
       })
     
 
--- cypress/support/factory/main.js
+++ cypress/support/factory/main.js
@@ -1,6 +1,3 @@
-import stringifyObject from 'stringify-object';
-import {prettyString} from '../factory/stringify'
-
 const faker = require("faker")
 
 const words = ["int", "string", "byte"]
@@ -8,16 +5,16 @@ const randomNumber = faker.datatype.number({ 'min': 0, 'max': words.length - 1 }
 let randomWord = words[randomNumber];
 
 let randomData = {
-    "name": faker.name.firstName() + '\t',
-    "topic": faker.random.alphaNumeric() + '/' +
-        faker.random.alphaNumeric() + '/' +
-        faker.random.alphaNumeric() + '/' +
-        faker.random.alphaNumeric() + '/' +
-        faker.random.word(),
+    "name": faker.name.firstName(),
+    "topic": "iot_device/topic",
     "payload": [
         ({"name": faker.random.alpha(5), "type": randomWord, "min": 1, "max": 100000, "default": 1})
     ]
 }
+
+let dataData = {"name":"Rodrick","topic":"iot_device/topic","payload":[{"name":"lazuf","type":"byte","min":1,"max":100000,"default":1}]};
+
+let finalData = {};
 
 // might have to pull out the payload as an array of objects, stringify that, remove "" from the ints and 
 // booleans, then put it back. Nothing else has quite done it right yet, despite multiple different attempts. 
@@ -25,8 +22,17 @@ let randomData = {
 // after we flatten data properly, we will move on to creating as much of it as we want. 
 export function dataCreation(fileName) {
 
+    // finalData = randomData.assign(dataData)
+    randomData = {...randomData, dataData }
+
+      cy.writeFile('cypress/data/files/' + fileName, JSON.stringify(randomData))
+
+}
+
+export function additionalDataCreation(fileName) {
     cy.writeFile('cypress/data/files/' + fileName, JSON.stringify(randomData))
 }
+
 
 
   
