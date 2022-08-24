6e655df8a0ef759fb777ba611edb6cdb801b67b1
James <jmorin1@phillips-connect.com>
2022-08-24 09:39:07 -0600

    pushing DF changes

--- cypress/support/factory/main.js
+++ cypress/support/factory/main.js
@@ -7,13 +7,27 @@ let randomWord = words[randomNumber];
 let randomData = {
     "name": faker.name.firstName(),
     "topic": "iot_device/topic",
-    "payload": [
-        ({"name": faker.random.alpha(5), "type": randomWord, "min": 1, "max": 100000, "default": 1})
-    ]
+    "payload": []
 }
 
-let randomPayload = {"name":"h-g-m-f-latitude", "type":"location",
-                     "lat":39.943436,"long":-74.915885,"radius":1000000}
+let randomPayload = [{
+    "name": faker.random.alpha(5) + '-latitude',
+    "static": false,
+    "type": "location",
+    "lat": faker.address.latitude(),
+    "long": faker.address.longitude(),
+    "radius": faker.datatype.number({ 'min': 1, 'max': 1000000 }
+    )
+},
+{
+    "name": faker.random.alpha(5),
+    "static": false,
+    "type": randomWord,
+    "min": 1,
+    "max": 100000,
+    "default": 1
+}
+]
 
 // JSON.stringify flattens the data into one line in a format that the data simulator can accept. 
 // However, the simulator can only accept one object per data file. So, for right now we will need
@@ -22,11 +36,24 @@ let randomPayload = {"name":"h-g-m-f-latitude", "type":"location",
 
 // still need a "Rosetta Stone" of payload explanations so we can mock them out and then create 
 // the different data types we might need. 
-export function dataCreation(fileName) {
+export function dataCreation(fileName, p = 1) {
+    const randomNumber = faker.datatype.number({ 'min': 0, 'max': randomPayload.length - 1 });
+    let nums = randomPayload[randomNumber];
 
-      cy.writeFile('cypress/data/files/' + fileName, JSON.stringify(randomData))
+    for (let i = 1; i <= p + 1; i++) {
+        randomData.payload.push[randomPayload[0]]
+    }
+
+    cy.log(randomPayload[0])
+    // writeToFile(fileName);
+
+    // cy.writeFile('cypress/data/files/' + fileName, JSON.stringify(randomData))
+    cy.writeFile('cypress/data/files/' + fileName, randomData)
 
 }
 
+function writeToFile (fileName) {
+    cy.writeFile('cypress/data/files/' + fileName, randomData)
+}
 
-  
+
