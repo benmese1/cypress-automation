ad9e6bcbb0e1349f34d42574928b3ccda1f92803
akalmykov <aleksandr.kalmikov@eu.jll.com>
2022-10-25 14:48:30 +0300

    rework done

--- cypress/e2e/loginSpec.cy.js
+++ cypress/e2e/loginSpec.cy.js
@@ -6,26 +6,16 @@ describe('Login', () => {
 
     it('Incorrect password test', () => {
         cy.login(Cypress.env('username'), 'incorrect');
-        cy.get('.errorMessage-customizable')
-            .should("be.visible")
-            .and("equal", 'Incorrect username or password.')
+        cy.contains('Incorrect username or password.')
     })
 
     it('Empty login test', () => {
         cy.login(' ', ' ');
-        cy.contains('.errorMessage-customizable')
-            .invoke('text')
-            .invoke('trim')
-            .should("be.visible")
-            .and("equal", 'The username you entered cannot be empty or contain only spaces')
+        cy.contains('The username you entered cannot be empty or contain only spaces')
     })
 
     it('Empty password test', () => {
         cy.login(Cypress.env('username'), ' ');
-        cy.contains('.errorMessage-customizable')
-            .invoke('text')
-            .invoke('trim')
-            .should("be.visible")
-            .and("equal", 'The password you entered cannot be empty or contain only spaces');
+        cy.contains('The password you entered cannot be empty or contain only spaces');
     })
 })
