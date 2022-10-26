2714c07d6cc0173f1d29ece372ccae82537899ba
akalmykov <aleksandr.kalmikov@eu.jll.com>
2022-10-26 12:31:14 +0300

    code cleanup

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -13,27 +13,10 @@ Cypress.Commands.add('createJSON', (fileName) => {
 
 
 Cypress.Commands.add('login', (user, pwd) => {
-    // const args = {
-    //     username: user, password: pwd,
-    //     client_id: Cypress.env('client_id'), redirect_uri: Cypress.config().baseUrl + '/login'
-    // }
-    // cy.visit("/login");
-    // cy.origin('https://devconnect1userpooldomain.auth.us-west-2.amazoncognito.com', {args}, ({
-    //                                                                                              username,
-    //                                                                                              password,
-    //                                                                                              client_id,
-    //                                                                                              redirect_uri,
-    //                                                                                          }) => {
-    //     cy.visit('/login?client_id=' + client_id + '&response_type=code&scope=email+openid+profile&redirect_uri=' + redirect_uri);
-    //     cy.get('.visible-lg #signInFormUsername').type(username);
-    //     cy.get('.visible-lg #signInFormPassword').type(password, {log: false});
-    //     cy.get('.visible-lg .btn-primary').click();
-    // });
     cy.visit("/login");
     cy.get('.visible-lg #signInFormUsername').type(user);
     cy.get('.visible-lg #signInFormPassword').type(pwd, {log: false});
     cy.get('.visible-lg .btn-primary').click();
-
 })
 
 //
