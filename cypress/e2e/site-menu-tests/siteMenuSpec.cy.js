b45faaaee601cca7725b4803c70d2619857b2031
Oleksandr Sulzhenko <osulzhenko1@phillips-connect.com>
2022-11-08 01:39:33 +0200

    Update

--- cypress/e2e/site-menu-tests/siteMenuSpec.cy.js
+++ cypress/e2e/site-menu-tests/siteMenuSpec.cy.js
@@ -11,13 +11,17 @@ describe('Landing page view test verification', () => {
 
     it('verify that menu item Asset List is linked to assets page', () => {
         cy.get('[data-testid="header"] [role="button"]').click()
-        cy.get('[data-testid="side-menu"]').contains('Asset List').click()
+        cy.get('[data-testid="side-menu"]')
+            .should('be.visible')
+            .contains('Asset List').click()
         cy.url().should('include', '/assets')
     })
 
     it('verify that menu item Asset Map is linked to the map page', () => {
         cy.get('[data-testid="header"] [role="button"]').click()
-        cy.get('[data-testid="side-menu"]').contains('Asset Map').click()
+        cy.get('[data-testid="side-menu"]')
+            .should('be.visible')
+            .contains('Asset Map').click()
         cy.url().should('include', '/map')
     })
 })
