a5cd13ccbdbf8bf002d811cf6a258055e34fc8e9
kchana1 <kchana1@phillips-connect.com>
2022-11-21 15:29:08 -0500

    Script updated

--- cypress/e2e/device-management/UI/loginDeviceManagement.cy.js
+++ cypress/e2e/device-management/UI/loginDeviceManagement.cy.js
@@ -5,18 +5,4 @@ describe('Login', () => {
         cy.get("[aria-label='account of current user']").should('be.visible');
     })
 
-    it('Incorrect password test', () => {
-        cy.login(Cypress.env('username'), 'incorrect', { cacheSession: false });
-        cy.contains('Incorrect username or password.')
-    })
-
-    it('Empty login test', () => {
-        cy.login(' ', ' ', { cacheSession: false });
-        cy.contains('The username you entered cannot be empty or contain only spaces')
-    })
-
-    it('Empty password test', () => {
-        cy.login(Cypress.env('username'), ' ', { cacheSession: false });
-        cy.contains('The password you entered cannot be empty or contain only spaces');
-    })
 })
