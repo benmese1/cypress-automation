ca12ae9ff3d3e6b4b2613a418292944b8fd3de84
Oleksandr Sulzhenko <osulzhenko1@phillips-connect.com>
2022-11-21 12:56:21 +0200

    Update

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -52,6 +52,7 @@ Cypress.Commands.add('logOutDB', () => {
 Cypress.Commands.add('dashboardMenu', () => {
     cy.get('[data-testid="header"] [role="button"]').click()
     cy.wait(1000)
+    cy.get('[data-testid="side-menu"]').should('be.visible')
     cy.get('[data-testid="header"] [role="button"]').click()
 })
 
