fb3ec71ee16cb54702765144141d0a0deef80df1
mbommasamudram <mbommasamudram1@phillips-connect.com>
2022-11-30 14:44:15 -0500

    create neworg changes

--- cypress/e2e/organization-management/create-new-org.cy.js
+++ cypress/e2e/organization-management/create-new-org.cy.js
@@ -1,3 +1,4 @@
+import org from '../../fixtures/createorg.json';
 describe('Create New Organization management',() =>{
     beforeEach(() => {
           cy.login(Cypress.env('username'), Cypress.env('password'), {cacheSession: false});
@@ -5,28 +6,37 @@ describe('Create New Organization management',() =>{
       })
   
       
-     it('Click on Create and verify the newly organization created' , () =>{
-        cy.navigateToLeftmenuItemsInHomePage('Organization');
-        cy.wait(1000);
-        /* On Navigating to Organization page application is loading
+     it('Click on Create and verify the newly organization created' , () =>{           
+      cy.dashboardMenu("My Organization");
+
+      //Name whicle creating sub org should be unique so appending  randowm 4 digit number to "SORG_"
+      let randomnumber = Math.floor((Math.random() * 10000) );
+
+      /* On Navigating to Organization page application is keep on loading( loading icon) 
          as a temporary fix to completed the automation following steps are performed
         reload  the page 
         login 
-        wait for certain time 
-        and navigate to organization        
-        */
-       //Name whicle creating sub org should be unique so appending  randowm 4 digit number to "SORG_"
-        let randomnumber = Math.floor((Math.random() * 10000) );
+        and navigate to organization     
+      */   
 
-        cy.reload();
-        cy.get('.visible-md > .modal-body > :nth-child(1) > :nth-child(1) > :nth-child(1) > .cognito-asf > :nth-child(2) > .btn').click();
-        cy.wait(10000);   
-        cy.navigateToLeftmenuItemsInHomePage('Organization');
-        cy.log("Create New button clicked");
-        cy.wait(10000);
-        cy.createNewOrganization('demo-root-org','SORG'+randomnumber,'custom','this is test org created by cypress automation','mani','Mountain Standard (GMT-7)');
+      cy.reload();
 
+      // click on confirm sign in
 
+      cy.get("[class='btn btn-primary submitButton-customizable']").eq(0).click({force: true});  
+      //wait for page loading
+
+      cy.waitForLoad()  
+
+      // Naviage to My Organization
+
+      cy.dashboardMenu("My Organization");
+      
+      // create new org
+      cy.log("Create New button clicked");
+        
+      cy.createNewOrganization(org[0].suborg.ParentOrganization,'SORG'+randomnumber,org[0].suborg.Type,org[0].suborg.Description,org[0].suborg.Brand,org[0].suborg.Timezone);
+  
       })
 
     })
