b1c9e62def7c05c83946089939bdd0b1093304b4
Tazim <tazimbanu.kolhar@evgo.com>
2022-12-02 16:57:28 -0500

    Add tests to verify asset status

--- cypress/e2e/mapview-tests/asset-status.cy.js
+++ cypress/e2e/mapview-tests/asset-status.cy.js
@@ -20,21 +20,21 @@ describe('Click on the canvas zoom the cluster and view the asset', () => {
         cy.get('div[data-testid="Moving - 0"]', { timeout: 10000 }).should('be.visible');
         cy.get('div[data-testid="Moving - 0"]').eq(0).click({force:true}).then(($btn) => {
             let elem1 = $btn.text();
-            cy.get('span[id="assets-count"]').then(($div) => { 
+            cy.get('#assets-count').then(($div) => { 
                 let elem2 = $div.text();
                 assert.equal(elem2.split(' ')[0], elem1.split(' ')[1]);
         }); });
 
         cy.get('div[data-testid="Parked - 1"]').eq(0).click({force:true}).then(($btn) => {
             let elem1 = $btn.text();
-            cy.get('span[id="assets-count"]').then(($div) => { 
+            cy.get('#assets-count').then(($div) => { 
                 let elem2 = $div.text();
                 assert.equal(elem2.split(' ')[0], elem1.split(' ')[1]);
         }); });
 
         cy.get('div[data-testid="Low - 2"]').eq(0).click({force:true}).then(($btn) => {
             let elem1 = $btn.text();
-            cy.get('span[id="assets-count"]').then(($div) => { 
+            cy.get('#assets-count').then(($div) => { 
                 let elem2 = $div.text();
 
                 assert.equal(elem2.split(' ')[0], elem1.split(' ')[1].substring(5,8));
@@ -42,21 +42,21 @@ describe('Click on the canvas zoom the cluster and view the asset', () => {
 
         cy.get('div[data-testid="Medium - 3"]').eq(0).click({force:true}).then(($btn) => {
             let elem1 = $btn.text();
-            cy.get('span[id="assets-count"]').then(($div) => { 
+            cy.get('#assets-count').then(($div) => { 
                 let elem2 = $div.text();
                 assert.equal(elem2.split(' ')[0], elem1.split(' ')[1].substring(5,8));
         }); });
 
         cy.get('div[data-testid="Large - 4"]').eq(0).click({force:true}).then(($btn) => {
             let elem1 = $btn.text();
-            cy.get('span[id="assets-count"]').then(($div) => { 
+            cy.get('#assets-count').then(($div) => { 
                 let elem2 = $div.text();
                 assert.equal(elem2.split(' ')[0], elem1.split(' ')[1].substring(6,9));
         }); });
 
         cy.get('div[data-testid="Mega - 5"]').eq(0).click({force:true}).then(($btn) => {
             let elem1 = $btn.text();
-            cy.get('span[id="assets-count"]').then(($div) => { 
+            cy.get('#assets-count').then(($div) => { 
                 let elem2 = $div.text();
                 assert.equal(elem2.split(' ')[0], elem1.split(' ')[1].substring(5,8));
         }); });
