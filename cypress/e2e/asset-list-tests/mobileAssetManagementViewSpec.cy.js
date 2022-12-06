9f3f376ed01d0728021bf0821f655448d8dba1c9
Oleksandr Sulzhenko <osulzhenko1@phillips-connect.com>
2022-12-06 12:44:36 +0200

    Added scrolling

--- cypress/e2e/asset-list-tests/mobileAssetManagementViewSpec.cy.js
+++ cypress/e2e/asset-list-tests/mobileAssetManagementViewSpec.cy.js
@@ -8,22 +8,25 @@ describe('Mobile Asset Management View verification', {retries: 0}, () => {
 
     it('Iphone portrait view', () => {
         cy.viewport('iphone-5', 'portrait')
+        cy.waitForLoad(2000)
         validatePageView()
     })
 
     it('Iphone landscape view', () => {
         cy.viewport('iphone-5', 'landscape')
+        cy.waitForLoad(2000)
         validatePageView()
     })
 
     it('Ipad portrait view', () => {
         cy.viewport('ipad-2', 'portrait')
+        cy.waitForLoad(2000)
         validatePageView()
     })
 
     it('Ipad landscape view', () => {
-        // type ViewportPreset = 'macbook-16' | 'macbook-15' | 'macbook-13' | 'macbook-11' | 'ipad-2' | 'ipad-mini' | 'iphone-xr' | 'iphone-x' | 'iphone-6+' | 'iphone-se2' | 'iphone-8' | 'iphone-7' | 'iphone-6' | 'iphone-5' | 'iphone-4' | 'iphone-3' | 'samsung-s10' | 'samsung-note9'
         cy.viewport('ipad-2', 'landscape')
+        cy.waitForLoad(2000)
         validatePageView()
     })
 
@@ -33,17 +36,18 @@ describe('Mobile Asset Management View verification', {retries: 0}, () => {
         cy.get('[data-testid="global-button-component"]').contains('Upload').should('be.visible')
         cy.get('[data-testid="global-button-component"]').contains('Export').should('be.visible')
         cy.get('[data-testid="global-button-component"]').contains('Add Asset').should('be.visible')
+
         // Validate table
+        cy.get(".MuiDataGrid-virtualScroller").scrollTo('topLeft')
         cy.get('[role="columnheader"][ aria-label="Company Name"]').should('be.visible')
 
-        // TODO scroll to bottom right corner
-        // cy.get('[role="columnheader"][ aria-label="Battery Power"]').should('be.visible')
+        cy.get(".MuiDataGrid-virtualScroller").scrollTo('topRight')
+        cy.get('[role="columnheader"][ aria-label="Battery Power"]').should('be.visible')
 
         validateDialog()
     }
 
     const validateDialog = () => {
-        cy.scrollTo('topLeft')
         cy.get('[data-testid="global-button-component"]')
             .contains('Upload')
             .click();
