b6ce7333136308c6a33f6c91d228190b41c4f0d1
SriDurga <sdurga1@phillips-connect.com>
2022-12-08 12:39:04 -0500

    org mgmt table changes

--- cypress/e2e/organization-management/org-mgmt-tableview.cy.js
+++ cypress/e2e/organization-management/org-mgmt-tableview.cy.js
@@ -1,43 +1,50 @@
 
-describe('Validate data on the Organization management table view', () => {
+describe('Verify Organization management table view', () => {
 
     beforeEach(() => {
-        cy.login(Cypress.env('username'), Cypress.env('password'), { cacheSession: false });
+        cy.login(Cypress.env('TESTusername'), Cypress.env('TESTpassword'), { cacheSession: false });
+        cy.waitForLoad()
         cy.get("[aria-label='account of current user']").should('be.visible');
+        cy.dashboardMenu('My Organization')
     })
 
-    it('Click on My Organization module and verify that fields present on My Organization management table view', () => {
-        cy.dashboardMenu('My Organization')
-        //verifying fields on the org mgmt table   
-        //Name field visibility
-        cy.get('[data-field="__tree_data_group__"]').should('be.visible').contains('Name')
-        //Description field visibility
-        cy.get('[data-field="description"]').should('be.visible').contains('Description')
-        //Timezone field visibility
-        cy.get('[data-field="time_zones"]').should('be.visible').contains('Timezone')
+    it('Click on My Organization module and verify the fields present in My Organization management table view', () => {         
+        //verifying fields on the org mgmt table  
+        //Org Name visibility
+        cy.get('[data-field="name"] .MuiDataGrid-columnHeaderTitle').should('be.visible').contains('Organization Name');
+        //Brand field visibility
+        cy.get('[data-field="brand"] .MuiDataGrid-columnHeaderTitle').should('be.visible').contains('Brand');
+        //TimeZone field visibiity
+        cy.get('[data-field="time_zones"] .MuiDataGrid-columnHeaderTitle').should('be.visible').contains('Timezone');
         //No. of Devices field visibility
-        cy.get('[data-field="asset_count"]').should('be.visible').contains('No. of Devices')
+        cy.get('[data-field="asset_count"] .MuiDataGrid-columnHeaderTitle').should('be.visible').contains('No. of Devices');
         //Updated field visibility
-        cy.get('[data-field="updated_date"]').should('be.visible').contains('Updated')
+        cy.get('[data-field="updated_date"] .MuiDataGrid-columnHeaderTitle').should('be.visible').contains('Updated')
         //Created field visibility
-        cy.get('[data-field="added_date"]').should('be.visible').contains('Created')
+        cy.get('[data-field="added_date"] .MuiDataGrid-columnHeaderTitle').should('be.visible').contains('Created')
 
     })
 
-    it('Click on My Organization module and verify that data present on My Organization management table view', () => {
-        cy.dashboardMenu('My Organization')
-        //verifying data on the org mgmt table   
-        //verify name of the organizaton
-        cy.xpath("//span[contains(text(), 'Philliips Connect')]").should('include.text', "Philliips Connect");
-        //Time zone data validation
-        cy.get('[data-id="3"] [data-field="time_zones"] .MuiDataGrid-cellContent').should('include.text', "EST");
-        //Updated validation
-        cy.get('[aria-colindex="6"][data-field="added_date"] .MuiDataGrid-cellContent').should('include.text', "11/1/2022")
-        //logout from the application
-        cy.logout()
+    it('Get the My organization name when user logged', () => {
+        cy.url().should('include', '/organizations')
+        cy.get('[data-testid="page"]').should('be.visible').contains('My Organization')
+        cy.get('[data-testid="page"] h3').then(function($elem) {
+            cy.log("My Organization name:"+ $elem.text())
+       })
+    }) 
 
+    it('verify export button is visible and downloading CSV file', () => {
+        cy.url().should('include', '/organizations')
+        cy.get('[aria-label="Export"]')
+            .should('be.visible')
+            .click()
+        cy.wait(600)
+        cy.get('[role="menuitem"]').contains('Download as CSV').click({force :true})
+        cy.readFile(downloadsPath + exportFilename).should('contain', csvFileHeader)
     })
 
-
+    const downloadsPath = 'cypress/downloads/'
+    const exportFilename = 'Phillips Connect.csv'
+    const csvFileHeader = 'Organization Name,Brand,Timezone,No. of Devices,Updated,Created'
 
 })
