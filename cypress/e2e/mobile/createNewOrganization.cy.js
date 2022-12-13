0c4c6f6303f111ea26b5d487ad2e7662c158c82c
mbommasamudram <mbommasamudram1@phillips-connect.com>
2022-12-13 12:23:29 -0500

    Createnew Org Mobile chages

---
+++ cypress/e2e/mobile/createNewOrganization.cy.js
@@ -0,0 +1,27 @@
+import org from '../../fixtures/createorg.json';
+const sizes = ['samsung-note9', 'ipad-2', 'iphone-8', [1170, 2532]];
+
+describe('Create New Organization managemen', () => {
+	sizes.forEach((size) => {
+		let randomnumber = Math.floor(Math.random() * 10000);
+		it(`Create New Organization managemen filed validations on ${size} screen`, () => {
+			if (Cypress._.isArray(size)) {
+				cy.viewport(size[0], size[1]);
+			} else {
+				cy.viewport(size);
+			}
+
+			cy.login(Cypress.env('TESTusername'), Cypress.env('TESTpassword'), { cacheSession: false });
+			cy.waitForLoad();
+			cy.get("[aria-label='account of current user']").should('be.visible');
+			cy.dashboardMenu('My Organization');
+			cy.createNewOrganization(
+				'SORG' + randomnumber,
+				org[0].suborg.Description,
+				org[0].suborg.Brand,
+				org[0].suborg.Timezone
+			);
+			cy.logout();
+		});
+	});
+});
