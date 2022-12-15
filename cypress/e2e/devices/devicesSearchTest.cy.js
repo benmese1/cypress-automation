b33f81031871b31c37bb650920815110b4680935
Ben <bmese1@phillips-connect.com>
2022-12-15 14:49:28 -0600

    fixed comment problems

--- cypress/e2e/devices/devicesSearchTest.cy.js
+++ cypress/e2e/devices/devicesSearchTest.cy.js
@@ -4,18 +4,18 @@ describe('Search and filter assets', () => {
 		cy.waitForLoad().openDevices();
 	});
 	it('Search device ID test', () => {
-		cy.searchDevices('861836059903026')
-			.get('[data-id="6386390ccb2ab377cdeaeab6"] > [data-field="imei"] > .MuiDataGrid-cellContent')
+		cy.searchDevices('6530')
+			.get('[data-id="63863905cb2ab377cdead8e7"] > [data-field="prd_cde"]')
 			.each(($item) => {
-				cy.wrap($item).should('have.text', '861836059903026');
+				cy.wrap($item).contains('6530');
 			});
 	});
 
 	it('Search devices by SIM test', () => {
-		cy.searchDevices('8944501910209408132')
+		cy.searchDevices('89445')
 			.get('[data-id="6386390ccb2ab377cdeaeab6"] > [data-field="sim_num"] > .MuiDataGrid-cellContent')
 			.each(($item) => {
-				cy.wrap($item).should('have.text', '8944501909193398598');
+				cy.wrap($item).contains('89445');
 			});
 	});
 	it('Search devices by Product Name test', () => {
