b6a94d2332d33ad9d99c8fc166d6482bf2567466
James <jmorin1@phillips-connect.com>
2022-12-16 16:26:05 -0700

    working on cicd

--- cypress/e2e/asset-list-tests/assetsListShowHideSpec.cy.js
+++ cypress/e2e/asset-list-tests/assetsListShowHideSpec.cy.js
@@ -1,37 +1,34 @@
 describe('Asset list table view tests', () => {
-    beforeEach(() => {
-        cy.login(Cypress.env('username'), Cypress.env('password'), { cacheSession: false })
-            .waitForLoad()
-            .dashboardMenu('Asset List');
-    });
+	beforeEach(() => {
+		cy.login(Cypress.env('username'), Cypress.env('password'), { cacheSession: false })
+			.waitForLoad()
+			.dashboardMenu('Asset List');
+	});
 
-    it('validate hide/show column in Asset list table', () => {
-        cy.get('[role="columnheader"][aria-label="Product"]')
-            .should('be.visible');
+	it('validate hide/show column in Asset list table', () => {
+		cy.get('[role="columnheader"][aria-label="Product"]').should('be.visible');
 
-        cy.get('[role="columnheader"][aria-label="Product"] [data-testid="TripleDotsVerticalIcon"]')
-            .click({force: true})
-            .get('[role="tooltip"]')
-            .should('be.visible')
-            .contains('Hide Columns')
-            .click();
+		cy.get('[role="columnheader"][aria-label="Product"] [data-testid="TripleDotsVerticalIcon"]')
+			.click({ force: true })
+			.get('[role="tooltip"]')
+			.should('be.visible')
+			.contains('Hide Columns')
+			.click();
 
-        // Check if a column is hidden
-        cy.get('[role="columnheader"][aria-label="Product"]')
-            .should('not.exist');
+		// Check if a column is hidden
+		cy.get('[role="columnheader"][aria-label="Product"]').should('not.exist');
 
-        // Show hidden column
-        cy.get('[data-testid="asset-table-toolbar-columns-btn"]')
-            .click()
-            .get('.MuiDataGrid-panelContent')
-            .should('be.visible')
-            .contains('Product')
-            .click()
+		// Show hidden column
+		cy.get('[data-testid="asset-table-toolbar-columns-btn"]')
+			.click()
+			.get('.MuiDataGrid-panelContent')
+			.should('be.visible')
+			.contains('Product')
+			.click();
 
-        cy.clickOutside()
+		cy.clickOutside();
 
-        // Check if a column is visible
-        cy.get('[role="columnheader"][aria-label="Product"]')
-            .should('be.visible');
-    })
-})
+		// Check if a column is visible
+		cy.get('[role="columnheader"][aria-label="Product"]').should('be.visible');
+	});
+});
--- cypress/e2e/mobile/orgmgmtview.cy.js
+++ cypress/e2e/mobile/orgmgmtview.cy.js
@@ -1,74 +1,60 @@
-const sizes = ['iphone-8', 'ipad-2', [1170, 2532], 'samsung-note9']
+const sizes = ['iphone-8', 'ipad-2', [1170, 2532], 'samsung-note9'];
 
 describe('Mobile Organization Management View verification', () => {
-  sizes.forEach((size) => {
-   it(`Organization management table and filed validations on ${size} screen`, () => {
-      if (Cypress._.isArray(size)) {
-        cy.viewport(size[0], size[1])
-      } else {
-        cy.viewport(size)
-      }
-      
-      cy.login(Cypress.env('TESTusername'), Cypress.env('TESTpassword'), { cacheSession: false });
-      cy.waitForLoad()
-      cy.get("[aria-label='account of current user']").should('be.visible');
-      cy.dashboardMenu('My Organization');
-      validatePageView();
-      validateCreateNew();
-    })
+	sizes.forEach((size) => {
+		it(`Organization management table and filed validations on ${size} screen`, () => {
+			if (Cypress._.isArray(size)) {
+				cy.viewport(size[0], size[1]);
+			} else {
+				cy.viewport(size);
+			}
 
-    /**
-     * Validate buttons and fields on the organization management table
-     */
-    const validatePageView = () => {
-        cy.get('[data-testid="page"]')
-          .should('be.visible')
-          .contains('My Organization');
+			cy.login(Cypress.env('TESTusername'), Cypress.env('TESTpassword'), { cacheSession: false });
+			cy.waitForLoad();
+			cy.get("[aria-label='account of current user']").should('be.visible');
+			cy.dashboardMenu('My Organization');
+			validatePageView();
+			validateCreateNew();
+		});
 
-        // Validate buttons
-        // Upload button visible
-        cy.get('[data-testid="global-button-component"]')
-          .contains('Upload')
-          .should('be.visible');
+		/**
+		 * Validate buttons and fields on the organization management table
+		 */
+		const validatePageView = () => {
+			cy.get('[data-testid="page"]').should('be.visible').contains('My Organization');
 
-        // Export button visible
-        cy.get('[data-testid="global-button-component"]')
-          .contains('Export')
-          .should('be.visible');
+			// Validate buttons
+			// Upload button visible
+			cy.get('[data-testid="global-button-component"]').contains('Upload').should('be.visible');
 
-        // Create New button visible
-        cy.get('[data-testid="global-button-component"]')
-          .contains('Create New')
-          .should('be.visible');
+			// Export button visible
+			cy.get('[data-testid="global-button-component"]').contains('Export').should('be.visible');
 
-        // Validate table
-        // Validate Organization name present on the table
-          cy.get('[role="grid"] div .MuiDataGrid-virtualScroller')
-            .scrollTo('topLeft',{ ensureScrollable: false })
-            .get('[role="columnheader"][data-field="name"]')
-            .should('be.visible');
+			// Create New button visible
+			cy.get('[data-testid="global-button-component"]').contains('Create New').should('be.visible');
 
-        // Validate Created field name present on the table
-          cy.get('[role="grid"] div .MuiDataGrid-virtualScroller')
-            .scrollTo('topRight',{ ensureScrollable: false })
-            .get('[role="columnheader"][data-field="added_date"]')
-            .should('be.visible');   
-    }
+			// Validate table
+			// Validate Organization name present on the table
+			cy.get('[role="grid"] div .MuiDataGrid-virtualScroller')
+				.scrollTo('topLeft', { ensureScrollable: false })
+				.get('[role="columnheader"][data-field="name"]')
+				.should('be.visible');
 
-    /**
-     * Validate Create new button and cancel the dialog once create new drawer opened
-     */
-    const validateCreateNew = () => {
-        cy.get('[data-testid="global-button-component"]')
-            .contains('Create New')
-            .click();
-        cy.get('[role="dialog"]')
-            .contains('Create New')
-            .should('be.visible');
-        cy.get('[data-testid="global-text-btn-component"]')
-            .contains('Cancel')
-            .click();
-        cy.logout();
-    }
-  })
-})
+			// Validate Created field name present on the table
+			cy.get('[role="grid"] div .MuiDataGrid-virtualScroller')
+				.scrollTo('topRight', { ensureScrollable: false })
+				.get('[role="columnheader"][data-field="added_date"]')
+				.should('be.visible');
+		};
+
+		/**
+		 * Validate Create new button and cancel the dialog once create new drawer opened
+		 */
+		const validateCreateNew = () => {
+			cy.get('[data-testid="global-button-component"]').contains('Create New').click();
+			cy.get('[role="dialog"]').contains('Create New').should('be.visible');
+			cy.get('[data-testid="global-text-btn-component"]').contains('Cancel').click();
+			cy.logout();
+		};
+	});
+});
