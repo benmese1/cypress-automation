680e61df87625e90f0e1fc18ad10bf49614dc8e6
akalmykov <Oleksandr.kalmykov1@phillips-connect.com>
2022-12-20 17:18:32 +0100

    code formatting

--- cypress/e2e/landing-page-tests/searchLocationSpec.cy.js
+++ cypress/e2e/landing-page-tests/searchLocationSpec.cy.js
@@ -16,42 +16,40 @@ describe('Location search tests', () => {
 			});
 	});
 
-    it('No suggestions for less than two chars test', () => {
+	it('No suggestions for less than two chars test', () => {
 		cy.get('[data-testid="location-selector-input"] input')
 			.type('at')
 			.get('.MuiAutocomplete-popper li span')
-            .should("not.be.visible");
+			.should('not.be.visible');
 	});
 
-    it('Get suggestions for less than two chars from recent search and remove from recent test', () => {
-        //search 'atlant'
+	it('Get suggestions for less than two chars from recent search and remove from recent test', () => {
+		//search 'atlant'
 		cy.get('[data-testid="location-selector-input"] input')
 			.type('atlant')
 			.get('.MuiAutocomplete-popper li span')
-            //select first result from autosuggestions list
-            .first()
-            .click()
-            .url()
-            //verify map page loaded
-            .should("contain", '/map')
-            .waitForLoad()
-            //back to dashboard page
-            .dashboardMenu('Dashboard')
-            .waitForLoad()
-            //search by two chars
-            .get('[data-testid="location-selector-input"] input')
-            .type('at')
-            .get('.MuiAutocomplete-popper li span')
-            .first()
-            .should("contain", "Atlanta, Georgia, USA")
-            //verify that user can see remove button in recent search
-            .get('.MuiAutocomplete-popper li button')
-            .should("be.visible")
-            .click()
-            //verify that no any suggestions displayed
-            .get('.MuiAutocomplete-popper')
-            .should("not.be.visible")
+			//select first result from autosuggestions list
+			.first()
+			.click()
+			.url()
+			//verify map page loaded
+			.should('contain', '/map')
+			.waitForLoad()
+			//back to dashboard page
+			.dashboardMenu('Dashboard')
+			.waitForLoad()
+			//search by two chars
+			.get('[data-testid="location-selector-input"] input')
+			.type('at')
+			.get('.MuiAutocomplete-popper li span')
+			.first()
+			.should('contain', 'Atlanta, Georgia, USA')
+			//verify that user can see remove button in recent search
+			.get('.MuiAutocomplete-popper li button')
+			.should('be.visible')
+			.click()
+			//verify that no any suggestions displayed
+			.get('.MuiAutocomplete-popper')
+			.should('not.be.visible');
 	});
-
-
 });
