7618faa2b7b51b265ab621c4cf6d87cf8590d16d
akalmykov <Oleksandr.kalmykov1@phillips-connect.com>
2022-12-23 12:48:43 +0100

    failed tests fixes

--- cypress/e2e/asset-list-tests/importDataSpec.cy.js
+++ cypress/e2e/asset-list-tests/importDataSpec.cy.js
@@ -3,41 +3,47 @@ import crypto from 'crypto';
 describe('Asset Management import verification', () => {
 	const filePath = 'cypress/downloads/assets.csv';
 	const csvHeader =
-		'Company Name,Asset ID,Asset Name,Device ID,Device IP,Latitude,Longitude,Ambient Temp,Internal Temp,Asset Type,Address,Trip Status,Dwell Time,Battery Power,Stale Time,Asset Tags\r\n';
-	const companyName = 'Phillips Connect';
+		'Asset ID,Asset Name,VIN,Category,# of Tires,# of Axles,Length,Door Type,Tags\r\n';
 	const assetId = crypto.randomBytes(16).toString('hex');
 	const assetName = 'CFQU' + Math.random() * 6;
-	const otherStaticData = 'NA,NA,34.52315,-115.54382,NA,NA,Container,"Will County, Illinois",Parked,-1,5.0,1.1';
-	const staleTime = new Date().toISOString();
-	const assetTag = 'test';
+	const otherStaticData = 'NA,Container,12,3,20,NA';
+	const assetTag = 'automation_test';
 
 	before(() => {
 		let generatedArrayData = [];
-		generatedArrayData.push(companyName, assetId, assetName, otherStaticData, staleTime, assetTag);
+		generatedArrayData.push(assetId, assetName, otherStaticData, assetTag);
 		cy.writeFile(filePath, csvHeader + generatedArrayData.join(','));
 	});
 
 	beforeEach(() => {
-		cy.login(Cypress.env('username'), Cypress.env('password'), { cacheSession: false }).waitForLoad().openAssetsList();
+		cy.login(Cypress.env('TESTusername'), Cypress.env('TESTpassword'), { cacheSession: false }).waitForLoad().openAssetsList();
 	});
 
 	it('Verify import button is visible and upload CSV file', () => {
 		cy.contains('button', 'Upload')
 			.should('be.visible')
 			.click()
-			.get('.MuiDialogTitle-root')
+			.get('.MuiDialog-paper h2')
 			.should('be.visible')
-			.and('have.text', 'Upload Assets CSV file')
-			.get('[name="file"]')
+			.and('contain.text', 'Upload file')
+			.get('#file-upload-input')
 			.selectFile(filePath, { force: true })
 			.get('.MuiDialogActions-root button')
-			.first()
+			.last()
 			.click()
-			.waitForLoad()
-			.get('[data-testid="snackbar-title"]')
-			.should('have.text', 'Assets were uploaded Successfully!')
+			// .get('.MuiCircularProgress-circle')
+			// .should("not.be.visible")
+			.get('.text-success')
+			.should("be.visible")
+			// .contains('1 asset has been added')
+			.get('[role="dialog"] [data-testid="global-button-component"]')
+			.should("be.visible")
+			.click()
+			// .waitForLoad()
+			// .get('[data-testid="snackbar-title"]')
+			// .should('have.text', 'Assets were uploaded Successfully!')
 			.searchAssets(assetName)
-			.get(".MuiDataGrid-row [data-field='asset_name'] div")
+			.get(".MuiDataGrid-row [data-field='name'] div")
 			.each(($item) => {
 				cy.wrap($item).should('have.text', assetName);
 			});
