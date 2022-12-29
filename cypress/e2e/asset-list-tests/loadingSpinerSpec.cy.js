dbebbcb90406012221967e65f4ca042a277b05ab
Oleks Sulzhenko <114674933+pholeks@users.noreply.github.com>
2022-12-29 18:07:52 +0200

    PRJIND-1311 Automation to check bugfix for loading spinner (#65)
    * PRJIND-1311 Automation to check bugfix for loading spinner
    
    * Update
    
    * Update

---
+++ cypress/e2e/asset-list-tests/loadingSpinerSpec.cy.js
@@ -0,0 +1,23 @@
+describe('Loading spinner verification', () => {
+    it('verify that the download icon from the landing page has disappeared on the asset list page', () => {
+        cy.login(Cypress.env('username'), Cypress.env('password'), {cacheSession: false});
+        cy.url()
+            .should('include', '/dashboard');
+        clickDashboardMenu();
+        cy.get('[data-testid="side-menu"]')
+            .should('be.visible')
+            .contains('Asset List')
+            .click();
+        cy.url()
+            .should('include', '/assets');
+        cy.get('[data-testid="spinner"]')
+            .should('not.exist');
+    });
+});
+
+const clickDashboardMenu = () => {
+    cy.wait(1)
+    cy.get('[data-testid="header"] [role="button"]')
+        .should('be.visible')
+        .click();
+};
