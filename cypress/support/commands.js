52fee23a002cd611fcc91fc653d654ef4b9cf2af
James <jmorin1@phillips-connect.com>
2022-12-30 10:01:46 -0700

    working on cicd

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -98,30 +98,6 @@ Cypress.Commands.add('createJSON', (fileName) => {
 	cy.writeFile('cypress/data/files/' + fileName, cy.fixture('chassis'));
 });
 
-Cypress.Commands.add('logInDB', () => {
-	// we would usually put the cy.visit() here as well, but left it in the beforeEach, just for training purposes
-
-	// finds login field and clicks on it
-	cy.get('#login2').click();
-
-	// fills out login information and clicks on log in button
-	// this timeout is unique to this selector. It will query for this selector for up to one second
-	cy.get('#loginusername', { timeout: 1000 }).type(Cypress.env('username'));
-	cy.get('#loginpassword', { timeout: 1000 }).type(Cypress.env('password'));
-	cy.get('[onclick="logIn()"]').click();
-	// a hard coded wait. The test will stop here for 1 sec, even if the element is available to interact with
-	cy.wait(1000);
-	// this is testing that the modal is no longer open
-	cy.get('#exampleModalLabel', { timeout: 2000 }).should('not.be.visible');
-});
-
-Cypress.Commands.add('logOutDB', () => {
-	// here is a hard coded wait. Technically a dynamic wait is better, but these can be useful still
-	cy.wait(1000);
-	// finds logout link and clicks on it
-	cy.get('#logout2', { timeout: 2000 }).click();
-});
-
 Cypress.Commands.add('assertFilterDates', (filter) => {
 	cy.get('[data-timestamp*="Z"]').each(($e) => {
 		cy.wrap($e)
