a6dfe299b06c7ab68be24413db5d9bcf9329ed32
James <jmorin1@phillips-connect.com>
2022-12-30 13:48:42 -0700

    working on cicd

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -94,14 +94,7 @@ Cypress.Commands.add('compareText', () => {
 //Click on the Dashboard menu
 
 Cypress.Commands.add('dashboardMenu', (menu) => {
-	const dashboardMenu = [
-		'Dashboard',
-		'Asset Map',
-		'Asset List',
-		'Devices',
-		'My Organization',
-		'User Management',
-	];
+	const dashboardMenu = ['Dashboard', 'Asset Map', 'Asset List', 'Devices', 'My Organization', 'User Management'];
 	cy.get('[data-testid="header"] [role="button"]')
 		.click()
 		.wait(500)
