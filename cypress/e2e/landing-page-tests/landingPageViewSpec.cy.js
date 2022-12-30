33205952d61ed01f53f84ef9634828c7b4d6c259
Oleksandr Sulzhenko <osulzhenko1@phillips-connect.com>
2022-12-30 11:28:00 -0500

    Update

--- cypress/e2e/landing-page-tests/landingPageViewSpec.cy.js
+++ cypress/e2e/landing-page-tests/landingPageViewSpec.cy.js
@@ -1,22 +1,11 @@
 describe('Landing page view test verification', () => {
 
-    it('verify Landing page for user without first name', () => {
-        loginAsUserWithoutFirstName();
-        cy.url().should('include', '/dashboard');
-        cy.contains('Welcome back!');
-    });
-
     it('verify Landing page for user with first name', () => {
         loginAsUserWithFirstName();
         cy.url().should('include', '/dashboard');
         cy.contains('Welcome back, James');
     });
 });
-
-const loginAsUserWithoutFirstName = () => {
-    cy.login(Cypress.env('username'), Cypress.env('password'), {cacheSession: false})
-        .waitForLoad();
-}
 
 const loginAsUserWithFirstName = () => {
     cy.login(Cypress.env('TESTusername'), Cypress.env('TESTpassword'), {cacheSession: false})
