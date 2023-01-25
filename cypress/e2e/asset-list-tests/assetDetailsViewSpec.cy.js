6afbcabd7d9feb85fdbdc5fe9ad37bbe5e6ddf7e
Viktoriia Skirko <vskirko1@phillips-connect.com>
2023-01-25 22:23:08 +0200

    PRJIND-1508 Remove consts in tests

--- cypress/e2e/asset-list-tests/assetDetailsViewSpec.cy.js
+++ cypress/e2e/asset-list-tests/assetDetailsViewSpec.cy.js
@@ -1,6 +1,10 @@
 const { log } = require("console");
 
+
+
 describe('Asset Management page -- details view verification', () => {
+
+
 	beforeEach(() => {
 		cy.login(Cypress.env('TESTusername'), Cypress.env('TESTpassword'), { cacheSession: false })
 			.waitForLoad()
@@ -8,6 +12,7 @@ describe('Asset Management page -- details view verification', () => {
 	});
 
 	it('Verify Asset Details View is collapsable', () => {
+		elements.usernameDropDown.click()
 		clickFirstRow();
 		cy.get('#details-summary').should('be.visible');
 		cy.get('#battery-block').should('exist');
@@ -18,93 +23,88 @@ describe('Asset Management page -- details view verification', () => {
 	});
 
 	it('Verify Critical battery state on Asset Details View', () => {
-		let criticalBatteryTestId = '[data-testid = "battery-svg-critical"]';
-
 		cy.addAssetsFilter('Battery Icon', 'starts with', '3.1');
 		cy.clickOutside();
 
 		//verify each 'Battery Icon' cells have appropriate icon
-		verifyBatteryIconsInAssetList(criticalBatteryTestId);
+		verifyBatteryIconsInAssetList('[data-testid = "battery-svg-critical"]');
 
 		clickFirstRow();
 
 		expandDetailsSection();
 
-		verifyBatteryIconIsDisplayed(criticalBatteryTestId);
+		verifyBatteryIconIsDisplayed('[data-testid = "battery-svg-critical"]');
 
 		//verify 'Battery Voltage' value
-		getBatteryVoltage().then(($val) => {
+		cy.get('[data-testid="asset-details-drawer-details-battery-voltage"]').then(($val) => {
 			expect(parseFloat($val.text())).to.be.lessThan(3.2)
 		});
 	});
 
 	it('Verify Alert battery state on Asset Details View', () => {
-		let alertBatteryTestId = '[data-testid = "battery-svg-alert"]';
-
+		
 		cy.addAssetsFilter('Battery Icon', 'starts with', '3.3');
 		cy.clickOutside();
 
 		//verify each 'Battery Icon' cells have appropriate icon
-		verifyBatteryIconsInAssetList(alertBatteryTestId);
+		verifyBatteryIconsInAssetList('[data-testid = "battery-svg-alert"]');
 
 		clickFirstRow();
 
 		expandDetailsSection();
 
-		verifyBatteryIconIsDisplayed(alertBatteryTestId);
+		verifyBatteryIconIsDisplayed('[data-testid = "battery-svg-alert"]');
 
 		//verify 'Battery Voltage' value
-		getBatteryVoltage().then(($val) => {
+		cy.get('[data-testid="asset-details-drawer-details-battery-voltage"]').then(($val) => {
 			expect(parseFloat($val.text())).to.be.greaterThan(3.21).lessThan(3.4)
 		});
 	});
 
 	it('Verify Warning battery state on Asset Details View', () => {
-		let warningBatteryTestId = '[data-testid = "battery-svg-warning"]';
 
 		cy.addAssetsFilter('Battery Icon', 'starts with', '3.5');
 		cy.clickOutside();
 
 		//verify each 'Battery Icon' cells have appropriate icon
-		verifyBatteryIconsInAssetList(warningBatteryTestId);
+		verifyBatteryIconsInAssetList('[data-testid = "battery-svg-warning"]');
 
 		clickFirstRow();
 
 		expandDetailsSection();
 
-		verifyBatteryIconIsDisplayed(warningBatteryTestId);
+		verifyBatteryIconIsDisplayed('[data-testid = "battery-svg-warning"]');
 
 		//verify 'Battery Voltage' value
-		getBatteryVoltage().then(($val) => {
+		cy.get('[data-testid="asset-details-drawer-details-battery-voltage"]').then(($val) => {
 			expect(parseFloat($val.text())).to.be.greaterThan(3.41).lessThan(3.6)
 		});
 	});
 
 
 	it('Verify Full battery state on Asset Details View', () => {
-		let fullBatteryTestId = '[data-testid = "battery-svg-full"]';
-
+	
 		cy.addAssetsFilter('Battery Icon', 'starts with', '3.9');
 		cy.clickOutside();
 
 		//verify each 'Battery Icon' cells have appropriate icon
-		verifyBatteryIconsInAssetList(fullBatteryTestId);
+		verifyBatteryIconsInAssetList('[data-testid = "battery-svg-full"]');
 
 		clickFirstRow();
 
 		expandDetailsSection();
 
-		verifyBatteryIconIsDisplayed(fullBatteryTestId);
+		verifyBatteryIconIsDisplayed('[data-testid = "battery-svg-full"]');
 
 		//verify 'Battery Voltage' value
-		getBatteryVoltage().then(($val) => {
+		cy.get('[data-testid="asset-details-drawer-details-battery-voltage"]').then(($val) => {
 			expect(parseFloat($val.text())).to.be.greaterThan(3.61)
 		});
 	});
  });
 
 const verifyBatteryIconsInAssetList = (expectedBatteryLocator) => {
-	cy.xpath("//*[@role='cell'][count(//*[@data-testid='column-header-battery-icon']//ancestor::*[@role='columnheader']/preceding-sibling::div) + 1]")
+	cy.xpath("//*[@role='cell'][count(//*[@data-testid='column-header-battery-icon']//ancestor::*[@role='columnheader']/preceding-sibling::*) + 1]")
 	.each(($item) => {
 		cy.wrap($item).find(expectedBatteryLocator).should('be.visible');
 	});
@@ -124,10 +124,6 @@ const expandDetailsSection = () => {
 					}
 				})
 		}));
-};
-
-const getBatteryVoltage = () => {
-	return cy.get('[data-testid="asset-details-drawer-details-battery-voltage"]')
 };
 
 const clickFirstRow = () => {
