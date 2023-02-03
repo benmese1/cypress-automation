e81215de1fd908cd6d5b0b33cb6db78a4f8f4f25
SriDurga <sdurga1@phillips-connect.com>
2023-02-03 15:45:44 -0500

    Org and User-SHowHide Columns changes

---
+++ cypress/e2e/user-management/userMgmtShowHideSpec.cy.js
@@ -0,0 +1,31 @@
+describe('User management table view tests', () => {
+	beforeEach(() => {
+		cy.login(Cypress.env('TESTusername'), Cypress.env('TESTpassword'), {
+			cacheSession: false,
+		}).waitForLoad();
+		cy.get("[aria-label='account of current user']").should('be.visible');
+		// Click User Management
+		cy.dashboardMenu('User Management');
+	});
+
+    it('Validate hide/show column in User management table', () => {
+		cy.get('[data-testid="column-header-username"]').should('be.visible');
+		//Click on the Menu w.r.t the column which want to Hide
+		cy.get('[role="columnheader"][aria-label="Username"] [data-testid="TripleDotsVerticalIcon"]').click({
+			force: true,
+		});
+		cy.get('[role="tooltip"]').should('be.visible');
+		cy.get('[data-testid="items-list-column-actions-menu-item-5"]').click();
+		// Check if a column is hidden
+		cy.get('[data-testid="column-header-username"]').should('not.exist');
+
+		// Show hidden column
+		cy.get('[data-testid="asset-table-toolbar-columns-btn"]').should('be.visible').click();
+		cy.get('[role="tooltip"]').should('be.visible').contains('Username').click();
+		cy.clickOutside();
+
+		// Check if a column is visible
+		cy.get('[data-testid="column-header-username"]').should('be.visible');
+	});
+
+});
