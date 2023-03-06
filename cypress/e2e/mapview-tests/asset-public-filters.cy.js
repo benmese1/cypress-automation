2e515958610cd570b3b310050d61cfdbbcafcb7c
tkolhar <tkolhar1@phillips-connect.com>
2023-03-06 15:04:58 -0500

    Added public filter tests

--- cypress/e2e/mapview-tests/asset-public-filters.cy.js
+++ cypress/e2e/mapview-tests/asset-public-filters.cy.js
@@ -17,7 +17,7 @@ describe('Create and remove public asset filters', () => {
 
 		// entering name in modal box and clicking save
 		cy.get('[data-testid="filter-textbox"]').type('asset filter 1');
-		cy.xpath('//p[contains(text(), "Save")]').click();
+		cy.get('[data-testid="dialog-action-btn"]').click();
 
 		//verifying success popup appeared
 		cy.get('[data-testid="snackbar-alert"]').contains('Success!');
@@ -26,28 +26,28 @@ describe('Create and remove public asset filters', () => {
 		cy.get('[data-testid="MoreVertIcon"]').click();
 
 		// click on update filter icon
-		cy.xpath('(//p[contains(text(), "asset filter 1")]/following::span[@title="Update"])[1]').click();
+		cy.get('[title="Update"]').eq(0).click();
 
 		//make the filter public by clicking shared
-		cy.get('[data-testid="filter-checkbox"]').check({ force: true });
+		cy.get('[data-indeterminate="false"]').check({ force: true });
 
 		// Confirm that filter is public and Shared is checked
-		cy.get('[data-testid="filter-checkbox"]').should('be.checked');
+		cy.get('[data-indeterminate="false"]').should('be.checked');
 
 		// click on update in the popup modal
-		cy.xpath('//p[contains(text(), "Update")]').click();
+		cy.get('[data-testid="dialog-action-btn"]').click();
 
 		//click on three filter dots upper right hand corner
 		cy.get('[data-testid="MoreVertIcon"]').click();
 
 		// click on update filter icon
-		cy.xpath('(//p[contains(text(), "asset filter 1")]/following::span[@title="Update"])[1]').click();
+		cy.get('[title="Update"]').eq(0).click();
 
 		// Uncheck the shared and make the filter private
-		cy.get('[data-testid="filter-checkbox"]').uncheck({ force: true });
+		cy.get('[data-indeterminate="false"]').uncheck({ force: true });
 
 		// click on update in the popup modal
-		cy.xpath('//p[contains(text(), "Update")]').click();
+		cy.get('[data-testid="dialog-action-btn"]').click();
 
 		//verifying success popup appeared
 		cy.get('[data-testid="snackbar-alert"]').contains('Success!');
@@ -56,27 +56,27 @@ describe('Create and remove public asset filters', () => {
 		cy.get('[data-testid="MoreVertIcon"]').click();
 
 		// click on update filter icon
-		cy.xpath('(//p[contains(text(), "asset filter 1")]/following::span[@title="Update"])[1]').click();
+		cy.get('[title="Update"]').eq(0).click();
 
 		// Confirm that filter is private and Shared is unchecked
-		cy.get('[data-testid="filter-checkbox"]').should('not.be.checked');
+		cy.get('[data-indeterminate="false"]').should('not.be.checked');
 
 		// click on Cancel in the popup modal
-		cy.xpath('//p[contains(text(), "Cancel")]').click();
+		cy.get('[data-testid="dialog-cancel-btn"]').click();
 
 		//click on three filter dots upper right hand corner
 		cy.get('[data-testid="MoreVertIcon"]').click();
 
 		// click on update filter icon
-		cy.xpath('(//p[contains(text(), "asset filter 1")]/following::span[@title="Delete"])[1]').click();
+		cy.get('[title="Delete"]').eq(0).click();
 
 		//try make the filter public by clicking shared
-		cy.get('[data-testid="filter-checkbox"]').check({ force: true });
+		cy.get('[data-indeterminate="false"]').check({ force: true });
 
 		// Confirm that filter is unchanged and Shared remains unchecked as we cannot make filter public by clicking delete icon
-		cy.get('[data-testid="filter-checkbox"]').should('be.unchecked');
+		cy.get('[data-indeterminate="false"]').should('be.disabled');
 
 		// click on Cancel in the popup modal
-		cy.xpath('//p[contains(text(), "Cancel")]').click();
+		cy.get('[data-testid="dialog-cancel-btn"]').click();
 	});
 });
