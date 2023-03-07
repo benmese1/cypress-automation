7b6697dd97491910854b282e6bb7ba85dc83b432
mbommasamudram <mbommasamudram1@phillips-connect.com>
2023-03-07 15:58:19 -0500

    Impersonation user changes

---
+++ cypress/e2e/user-impersonation/userImpersonationSpec.cy.js
@@ -0,0 +1,33 @@
+import impersonationdata from '../../fixtures/impersonation.json';
+
+describe('Verify from Superadmin able to see Impersonate user option in popover menu, search user and impersonation user name after switch over', () => {
+	beforeEach(() => {
+		cy.login(Cypress.env('TESTusername'), Cypress.env('TESTpassword'), {
+			cacheSession: false,
+		}).waitForLoad();
+		cy.get("[aria-label='account of current user']").should('be.visible');
+	});
+
+	it('verify from Superadmin able to see Impersonate user option in popover menu, search user and impersonation user name after switch over, Verify User Management table data from Imersonate user,Verify organization table data from Imersonate user and Stop Impersonation ', () => {
+		cy.myAcount_ImpersonateUser(
+			impersonationdata[0].impersonation.username,
+			impersonationdata[0].impersonation.fnamelname
+		);
+
+		//Verify targeted user, user management table has the data
+		cy.dashboardMenu('User Management');
+		cy.get('[data-testid="management-user-management"]', { timeout: 2000 }).should('exist');
+		cy.get('[data-testid="btn-sub-header-action-Create New"]', { timeout: 2000 }).should('exist');
+		cy.verifyMyOrganizationTableView();
+		cy.waitForLoad();
+
+		//Verify targeted user organization table has the data
+		cy.dashboardMenu('My Organization');
+		cy.get('[data-testid="management-my-organization"]', { timeout: 2000 }).should('exist');
+		cy.get('[data-testid="btn-sub-header-action-Create New"]', { timeout: 1000 }).should('exist');
+		cy.verifyMyOrganizationTableView();
+
+		//Stop Impersonating
+		cy.myAcount_StopImpersonating();
+	});
+});
---
+++ cypress/support/commands/myAccount.js
@@ -0,0 +1,19 @@
+//Method Name :myAcount_ImpersonateUser
+//Used to do user impersonation
+//Params impersonation username
+Cypress.Commands.add('myAcount_ImpersonateUser', (impersonationusername, impersonationfullname) => {
+	cy.get("[aria-label='account of current user']").click();
+	cy.get('[data-testid="SupervisorAccountOutlinedIcon"]').should('be.visible').click();
+	cy.get('[id="impersonate-user-search"]').wait(3000).type(impersonationusername);
+	cy.get('p').contains(impersonationfullname).click();
+	cy.waitForLoad();
+	cy.get('[data-testid="logged-in-username"]').should('be.visible').contains(impersonationusername);
+});
+
+//Method Name :myAcount_StopImpersonating"
+//Used to StopImpersonating
+//Params NA
+Cypress.Commands.add('myAcount_StopImpersonating', () => {
+	cy.wait(3000).get('[data-testid="logged-in-username"]').click();
+	cy.get('[data-testid="global-text-btn-component"]').should('be.visible').click();
+});
