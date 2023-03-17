7c4cdb90a8c85c7b2515bc68274d8a940e52a4d8
tkolhar <tkolhar1@phillips-connect.com>
2023-03-17 18:28:45 -0400

    fixed the asset status

--- cypress/e2e/asset-map/asset-status.cy.js
+++ cypress/e2e/asset-map/asset-status.cy.js
@@ -1,20 +1,14 @@
 // @team1
 const { time } = require('console');
 
-describe('Click on the canvas zoom the cluster and view the asset', () => {
-	it('Success canvas click test and verify and view asset details', () => {
-		// Login to Dev Environment
-		cy.login(Cypress.env('username'), Cypress.env('password'), { cacheSession: false });
+describe('Click on the canvas zoom the cluster and view the asset status', () => {
+	beforeEach(() => {
+		cy.login(Cypress.env('TESTusername'), Cypress.env('TESTpassword'), { cacheSession: false })
+			.waitForLoad()
+			.dashboardMenu('Asset Map');
+	});
 
-		// Assert the user is visible
-		cy.get("[aria-label='account of current user']").should('be.visible');
-
-		// Click Asset Maps
-		cy.dashboardMenu('Asset Map');
-
-		// Wait for the map to reload
-		cy.mapWait();
-
+	it('Success canvas click test and verify and view asset details and verify the asset status', () => {
 		cy.get('[data-testid="filter-item-Status"]').click({ force: true });
 		cy.get('[data-testid="Moving - 0"]', { timeout: 10000 }).should('be.visible');
 		cy.get('[data-testid="Moving - 0"]')
@@ -22,6 +16,7 @@ describe('Click on the canvas zoom the cluster and view the asset', () => {
 			.click({ force: true })
 			.then(($btn) => {
 				let elem1 = $btn.text();
+				cy.get('#assets-count', { timeout: 20000 }).should('be.visible');
 				cy.get('#assets-count').then(($div) => {
 					let elem2 = $div.text();
 					assert.equal(elem2.split(' ')[0], elem1.split(' ')[1]);
@@ -33,6 +28,7 @@ describe('Click on the canvas zoom the cluster and view the asset', () => {
 			.click({ force: true })
 			.then(($btn) => {
 				let elem1 = $btn.text();
+				cy.get('#assets-count', { timeout: 20000 }).should('be.visible');
 				cy.get('#assets-count').then(($div) => {
 					let elem2 = $div.text();
 					assert.equal(elem2.split(' ')[0], elem1.split(' ')[1]);
@@ -44,6 +40,7 @@ describe('Click on the canvas zoom the cluster and view the asset', () => {
 			.click({ force: true })
 			.then(($btn) => {
 				let elem1 = $btn.text();
+				cy.get('#assets-count', { timeout: 20000 }).should('be.visible');
 				cy.get('#assets-count').then(($div) => {
 					let elem2 = $div.text();
 
@@ -56,6 +53,7 @@ describe('Click on the canvas zoom the cluster and view the asset', () => {
 			.click({ force: true })
 			.then(($btn) => {
 				let elem1 = $btn.text();
+				cy.get('#assets-count', { timeout: 20000 }).should('be.visible');
 				cy.get('#assets-count').then(($div) => {
 					let elem2 = $div.text();
 					assert.equal(elem2.split(' ')[0], elem1.split(' ')[1].substring(5, 8));
@@ -78,6 +76,7 @@ describe('Click on the canvas zoom the cluster and view the asset', () => {
 			.click({ force: true })
 			.then(($btn) => {
 				let elem1 = $btn.text();
+				cy.get('#assets-count', { timeout: 20000 }).should('be.visible');
 				cy.get('#assets-count').then(($div) => {
 					let elem2 = $div.text();
 					assert.equal(elem2.split(' ')[0], elem1.split(' ')[1].substring(5, 8));
