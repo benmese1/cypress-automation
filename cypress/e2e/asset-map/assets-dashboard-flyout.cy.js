11dfb92c1901f0a00a9f0d7ed46fbcf862016dea
tkolhar <tkolhar1@phillips-connect.com>
2023-03-20 13:05:13 -0400

    Fix Asset dashboard flyout test

--- cypress/e2e/asset-map/assets-dashboard-flyout.cy.js
+++ cypress/e2e/asset-map/assets-dashboard-flyout.cy.js
@@ -1,25 +1,20 @@
 // @team1
 describe('Click on the asset dashboard flyout ', () => {
+	beforeEach(() => {
+		cy.login(Cypress.env('TESTusername'), Cypress.env('TESTpassword'), { cacheSession: false })
+			.waitForLoad()
+			.dashboardMenu('Asset Map');
+	});
+
 	it('Success click on asset dashboard flyout and close it', () => {
-		// Login to Dev Environment
-		cy.login(Cypress.env('username'), Cypress.env('password'), { cacheSession: false });
-
-		// Assert the user is visible
-		cy.get("[aria-label='account of current user']").should('be.visible');
-
-		// Click Asset Maps
-		cy.dashboardMenu('Asset Map');
-
-		// Wait for the map to reload
-		cy.mapWait();
-
 		//Click on the asset and check if flyout opens
+		cy.get('[data-testid*="asset-"]', { timeout: 20000 }).eq(0).should('be.visible');
 		cy.get('[data-testid*="asset-"]')
 			.eq(0)
 			.click({ force: true })
 			.then(() => {
 				//Click on close button to close the flyout
-				cy.get('[data-testid="asset-panel-close-btn"]').click({ force: true });
+				cy.get('[data-testid="CloseIcon"]').eq(0).click({ force: true });
 			});
 	});
 });
