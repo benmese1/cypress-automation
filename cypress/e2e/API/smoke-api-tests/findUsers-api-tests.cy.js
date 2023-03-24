c0268222c28c794ba17c407f8513c633a650c4a0
kchana1 <kchana1@phillips-connect.com>
2023-03-24 12:27:13 -0400

    findUsers file added

--- cypress/e2e/API/smoke-api-tests/findUsers-api-tests.cy.js
+++ cypress/e2e/API/smoke-api-tests/findUsers-api-tests.cy.js
@@ -1,42 +1,27 @@
-// API test for the find all Users by generating JWT on the fly : Dev only
+// API test for the find all Users by generating JWT on the fly
 /// <reference types="cypress" />
-
-it(
-	'Send an API request to find all Users',
-	{
-		// Multiple attempts as the API is intermittent to respond at once
-		retries: {
-			runMode: 2,
-			openMode: 2,
-		},
-	},
-	() => {
-		// https://on.cypress.io/request
-		// Method and URI to be specified here along with query and path params
-
-		cy.request({
-			method: 'POST',
-			url: Cypress.env('cognito_uri'),
-			headers: {
-				'X-Amz-Target': Cypress.env('X-Amz-Target'),
-				'Content-Type': Cypress.env('Content-Type'),
-			},
-			body: {
-				AuthFlow: Cypress.env('AuthFlow'),
-				ClientId: Cypress.env('ClientId'),
-				AuthParameters: { USERNAME: Cypress.env('TESTusername'), PASSWORD: Cypress.env('TESTpassword') },
-			},
-		}).should((response) => {
-			expect(response.status).to.eq(200);
-			expect(response.body.AuthenticationResult).to.have.property('IdToken');
-			expect(response.body.AuthenticationResult).to.have.property('ExpiresIn').to.eq(3600);
-			// Token is stored and will be consumed in graphQL API call
-			global.token = response.body.AuthenticationResult.IdToken;
-			cy.log('Token = ' + token);
+describe('find all Users api tests1', () => {
+	let cookieValue;
+	beforeEach(() => {
+		cy.generateAPIToken();
+		cy.getCookie('apitoken').then((cookie) => {
+			cookieValue = cookie.value;
 		});
-
-		// GraphQL query to findUsers
-		const query = `
+	});
+	it(
+		'Send an API request to find all Users',
+		{
+			// Multiple attempts as the API is intermittent to respond at once
+			retries: {
+				runMode: 2,
+				openMode: 2,
+			},
+		},
+		() => {
+			// https://on.cypress.io/request
+			// Method and URI to be specified here along with query and path params
+			// GraphQL query to findUsers
+			const query = `
 		query MyQuery {
 			findUsers {
 			  sub
@@ -53,20 +38,21 @@ it(
 			}
 		  }
 		  
-		 `;
+		`;
 
-		cy.request({
-			method: 'POST',
-			url: Cypress.env('graphQL_uri'),
-			headers: {
-				Authorization: global.token,
-				'Content-Type': 'application/json',
-			},
-			body: { query },
-		}).then((res) => {
-			expect(res.status).to.eq(200);
-			expect(res.body.data.findUsers[0]).to.have.property('firstName');
-			cy.log(res.body);
-		});
-	}
-);
+			cy.request({
+				method: 'POST',
+				url: Cypress.env('graphQL_uri'),
+				headers: {
+					Authorization: cookieValue,
+					'Content-Type': 'application/json',
+				},
+				body: { query },
+			}).then((res) => {
+				expect(res.status).to.eq(200);
+				expect(res.body.data.findUsers[0]).to.have.property('firstName');
+				cy.log(res.body);
+			});
+		}
+	);
+});
