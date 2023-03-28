a33f2ad51960112cbd305bacc92c922c7f5f0b75
Paul Mun <pmun1@phillips-connect.com>
2023-03-28 10:13:57 -0400

    fixing abbreviated details tests

--- cypress/e2e/asset-map/abbreviated-details.cy.js
+++ cypress/e2e/asset-map/abbreviated-details.cy.js
@@ -1,29 +1,23 @@
 // @team1
 describe('Click on the canvas zoom the cluster and view the asset', () => {
+	beforeEach(() => {
+		cy.login(Cypress.env('TESTusername'), Cypress.env('TESTpassword'), { cacheSession: false })
+			.waitForLoad()
+			.dashboardMenu('Asset Map');
+		});
+
 	it('Success canvas click test and verify and view asset details', () => {
-		// Login to Dev Environment
-		cy.login(Cypress.env('TESTusername'), Cypress.env('TESTpassword'), { cacheSession: false });
-
-		// Assert the user is visible
-		cy.get("[aria-label='account of current user']").should('be.visible');
-
-		// Click Asset List View
-		cy.get('p').contains('Asset List View 1').click({ force: true });
-
-		// Wait for the map to reload
-		cy.mapWait();
-
 		// Click on the Asset
-		cy.xpath("//p[contains(text(), 'CFQU119342')]").click({ force: true });
+		cy.get('[data-testid*="asset-"]', {timeout: 20000}).eq(0).should('be.visible').click({ force: true });
 
 		// Verify Asset Name
-		cy.xpath("//p[contains(text(), 'CFQU119342')]").should('include.text', 'CFQU119342');
+		cy.xpath("//p[contains(text(), '378977')]").should('include.text', '378977');
 
 		// Verify City State
-		cy.xpath("//span[contains(text(), 'Atlanta, Georgia')]").should('include.text', 'Atlanta, Georgia');
+		cy.xpath("//span[contains(text(), 'Hamilton County, TN')]").should('include.text', 'Hamilton County, TN');
 
 		// Verify Time Stamp
-		cy.xpath("//span[contains(text(), '13 hours')]").eq(0).should('include.text', '13 hours');
+		cy.xpath("//span[contains(text(), '2 minutes')]").eq(0).should('include.text', '2 minutes');
 
 		// Expand the Additional tab
 		cy.get("[class='MuiTouchRipple-root css-w0pj6f']").eq(0).click({ force: true });
